aa6b897ecaccd17a126bb2849581e918a557bece
rajbir21-droid <banerjeerajbir2888@gmail.com>
2021-05-16 20:27:55 +0530

    Updates

---
+++ precache-manifest.fe88eb481dfaa8062092c6b6d7747cb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0286360ca8f75d366663",
+    "url": "/FaceRecognition/static/css/main.582d9ef3.chunk.css"
+  },
+  {
+    "revision": "0286360ca8f75d366663",
+    "url": "/FaceRecognition/static/js/main.cd916484.chunk.js"
+  },
+  {
+    "revision": "4e809583f6f234bdba73",
+    "url": "/FaceRecognition/static/js/runtime~main.728cdf97.js"
+  },
+  {
+    "revision": "11d0a46b2cbbd45db470",
+    "url": "/FaceRecognition/static/css/2.864579fa.chunk.css"
+  },
+  {
+    "revision": "11d0a46b2cbbd45db470",
+    "url": "/FaceRecognition/static/js/2.f7ea3ce6.chunk.js"
+  },
+  {
+    "revision": "a527ee9a3cc6cd3f34ff",
+    "url": "/FaceRecognition/static/js/3.a722ff0c.chunk.js"
+  },
+  {
+    "revision": "599df29e6ea6c06e3f89909c21c2ef22",
+    "url": "/FaceRecognition/index.html"
+  }
+];
